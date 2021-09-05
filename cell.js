cd0db01ef2a1c05d5b96320c8bab854f227b5c96
AgungAndre01 <agungandre687@gmail.com>
2021-09-05 12:18:07 +0800

    first commit

---
+++ cell.js
@@ -0,0 +1,100 @@
+class Cell {
+	constructor(i, j, w, trap) {
+		this.w = w;
+		this.i = i;
+		this.j = j;
+		this.x = j * w;
+		this.y = i * w;
+		this.traps = -1;
+		this.revealed = false;
+		this.trap = trap;
+	}
+	show() {
+		if (this.revealed && !this.trap) {
+			fill(38, 34, 34);
+		} else {
+			fill(242, 241, 235);
+		}
+
+		stroke(0);
+		rect(this.x, this.y, this.x + this.w, this.y + this.w);
+		if (this.revealed) {
+			if (this.trap) {
+				fill(222, 64, 47);
+				circle(
+					this.x + this.w * 0.5,
+					this.y + this.w * 0.5,
+					this.w * 0.5
+				);
+			} else {
+				// textAlign(CENTER);
+				fill(255);
+				textSize(this.w * 0.5);
+				text(this.traps, this.x + this.w * 0.5, this.y + this.w * 0.5);
+			}
+		}
+	}
+	floodFill() {
+		for (let i = -1; i <= 1; i++) {
+			for (let j = -1; j <= 1; j++) {
+				let iCurr = this.i + i;
+				let jCurr = this.j + j;
+				if (iCurr < 0 || iCurr >= board.length) {
+					continue;
+				}
+				if (jCurr < 0 || jCurr >= board[0].length) {
+					continue;
+				}
+				if (!board[iCurr][jCurr].revealed) {
+					revealed++;
+					// await sleep(1);
+					board[iCurr][jCurr].reveal();
+				}
+			}
+		}
+	}
+	/**
+	 *
+	 * @param {boolean} noFloodFill disable flood fill
+	 */
+	reveal(noFloodFill) {
+		this.revealed = true;
+		if (!noFloodFill) {
+			if (this.traps == 0) {
+				this.floodFill();
+			} else if (this.trap) {
+				gameOver();
+			}
+		}
+	}
+	countTraps() {
+		if (this.trap) {
+			return;
+		}
+		let count = 0;
+		for (let i = -1; i <= 1; i++) {
+			for (let j = -1; j <= 1; j++) {
+				let iCurr = this.i + i;
+				let jCurr = this.j + j;
+				if (iCurr < 0 || iCurr >= board.length) {
+					continue;
+				}
+				if (jCurr < 0 || jCurr >= board[0].length) {
+					continue;
+				}
+				if (board[iCurr][jCurr].trap) {
+					count++;
+				}
+			}
+		}
+		this.traps = count;
+	}
+	contains(x, y) {
+		return (
+			x > this.x &&
+			x < this.x + this.w &&
+			y > this.y &&
+			y < this.y + this.w
+		);
+	}
+}
---
+++ sketch.js
@@ -0,0 +1,123 @@
+let boardSize;
+var board;
+let w;
+let trapChance; // percent
+let trapCount; // note: this is not the final trap count
+var revealed; // keep track of revealed count
+let boardWidth;
+
+function setting(bs, tc) {
+	if (!bs) {
+		bs = 10;
+	}
+	if (!tc) {
+		tc = 10;
+	}
+	boardSize = bs;
+	boardWidth = windowWidth > windowHeight ? windowHeight : windowWidth;
+	console.log(boardSize);
+	board = new Array(boardSize);
+	trapChance = tc;
+	trapCount = 0;
+	revealed = 0;
+	w = width / boardSize;
+	for (let i = 0; i < boardSize; i++) {
+		board[i] = new Array(boardSize);
+		for (let j = 0; j < boardSize; j++) {
+			let isTrap = random(1) < trapChance / 100;
+			if (isTrap) {
+				trapCount++;
+			}
+			board[i][j] = new Cell(i, j, w, isTrap);
+			board[i][j].revealed = false;
+		}
+	}
+
+	for (let row of board) {
+		for (let cell of row) {
+			if (!cell.trap) {
+				cell.countTraps();
+			}
+		}
+	}
+}
+
+function setup() {
+	boardWidth = windowWidth > windowHeight ? windowHeight : windowWidth;
+	createCanvas(boardWidth - boardWidth * 0.2, boardWidth - boardWidth * 0.2);
+	setting(10, 10);
+	let bsInput = document.getElementById("board_size");
+	let tcInput = document.getElementById("trap_chance");
+
+	document.getElementById("reload").onclick = () => {
+		setting(bsInput.value, tcInput.value);
+		redraw();
+	};
+	noLoop();
+}
+
+function getRemaining() {
+	return boardSize * boardSize - trapCount - revealed;
+}
+
+function changeTextById(id, text) {
+	document.getElementById(id).innerHTML = text;
+}
+
+function gameOver() {
+	revealAll();
+	changeTextById("game_status", "You lose!");
+}
+
+function revealAll() {
+	for (let row of board) {
+		for (let cell of row) {
+			cell.reveal(true);
+		}
+	}
+	redraw();
+}
+
+function win() {
+	revealAll();
+	changeTextById("game_status", "You win!");
+}
+
+function mousePressed() {
+	if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) return;
+	let found = false;
+	for (let row of board) {
+		for (let cell of row) {
+			if (cell.contains(mouseX, mouseY)) {
+				if (!cell.revealed) {
+					cell.reveal();
+					if (!cell.trap) {
+						revealed++;
+						changeTextById(
+							"remaining",
+							getRemaining() + " remaining.."
+						);
+						redraw();
+					}
+					if (revealed == boardSize * boardSize - trapCount) {
+						win();
+					}
+				}
+				found = true;
+				break;
+			}
+		}
+		if (found) {
+			break;
+		}
+	}
+}
+
+function draw() {
+	background(100);
+	for (let row of board) {
+		for (let cell of row) {
+			cell.show();
+		}
+	}
+}
